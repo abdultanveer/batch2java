b23f7678fe6bc12a3789ed05afa674f26dec698d
abdul <abdul.tanveer@gmail.com>
2019-07-15 20:41:26 +0530

    class and object

--- Batch2/src/HelloWorld.java
+++ Batch2/src/HelloWorld.java
@@ -2,6 +2,10 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		Student abdul ; //declaration
+		abdul = new Student(); //instanstiation
+		abdul.name = "ansari";    //instance variable = value in the box partition --- initialization
+		Student alfred = new Student();   //new will allocate mem in RAM
 		System.out.println("hello world");
 		
 		GST gst;//a variable reference is created on the stack m/y
@@ -13,5 +17,10 @@ public class HelloWorld {
 		System.out.println("gst="+gstTax);
 		
 	}
+	
+	//declaration
+	//instanstiation
+	//initialization
+	
 
 }
--- Batch2/src/Student.java
+++ Batch2/src/Student.java
@@ -4,5 +4,12 @@ public class Student {
 	int age;
 	float stipend;
 	int marks;
+	
+	
+	private  int getStipend() {
+		return 1000;
+	}
+	
+	private void askDoubt(String doubt) {}
 
 }
